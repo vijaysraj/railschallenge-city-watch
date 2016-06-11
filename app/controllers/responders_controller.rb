cf2ba4938c01022d54353f9a8064e1c7d8117b62
Vijay <vijay@Vijays-MacBook-Pro.local>
2016-06-11 12:36:33 +0530

    rubocop fixes

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,18 +1,18 @@
 class ResponderController < ApplicationController
 
-	def create
-		@responder = Responder.new(responder_params)	
-		if responder.save
-			render json: {responder: @responder}, status: 201
-		else
-			render json: {message: @responder.errors}, status: 201
-		end
-	end
+  def create
+    @responder = Responder.new(responder_params)  
+    if responder.save
+      render json: {responder: @responder}, status: 201
+    else
+      render json: {message: @responder.errors}, status: 201
+    end
+  end
 
-	private
+  private
 
-	def responder_params
+  def responder_params
     params.require(:responder).permit(:type, :name, :capacity)
-	end
+  end
 
 end
